f57677b171d19ee96411223094b2d4726df9bb9b
ShadowIterator <ShadowIterator@hotmail.com>
2018-12-11 07:20:03 +0800

    before add test cases

--- backend/testcases/exampletest.py
+++ backend/testcases/exampletest.py
@@ -1,7 +1,7 @@
 # from basetestcase.basetestcase import BaseTestCase, async_aquire_db
 import unittest
 import tornado.testing
-from ..basetestcase.basetestcase import BaseTestCase, async_aquire_db
+from basetestcase.basetestcase import BaseTestCase, async_aquire_db
 
 class ExampleTestCase(BaseTestCase):
 
