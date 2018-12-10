58ac6ba3be59ab55bce0c934357974d69ab6946b
hongfz <hongfz16@163.com>
2018-12-10 21:07:20 +0800

    finish admin basic display

--- frontend/src/admin-component/admin-page.js
+++ frontend/src/admin-component/admin-page.js
@@ -17,23 +17,32 @@ class AdminTable extends Component {
         };
         this.data = [];
         this.updateTable = this.updateTable.bind(this);
-        this.props.columns.push(
-            {title: 'Action', dataIndex: 'action', key: 'action', fixed: 'right', render: (text, record) =>
-                    <span>
+    }
+    updateTable = function(page) {
+        console.log(this.state);
+        const column_len = this.props.columns.length;
+        if(this.props.columns[column_len-1].title!=='操作') {
+            let fixed = 'right';
+            if(this.props.current_tab === 'Homework' || this.props.current_tab === 'Notices') {
+                console.log("fixed: null");
+                fixed = null;
+            }
+            this.props.columns.push(
+                {
+                    title: '操作', dataIndex: 'action', key: 'action', fixed: fixed, render: (text, record) =>
+                        <span>
                         <Button onClick={() => {
                             ajax_post(this.props.delete_api, {id: record.id}, this, (that, result) => {
-                                if(result.data.code===1) {
+                                if (result.data.code === 1) {
                                     alert("Delete Failed!");
                                 } else {
                                     this.updateTable(this.state.page);
                                 }
                             });
-                        }}>Delete</Button>
+                        }}>删除</Button>
                     </span>
-            });
-    }
-    updateTable = function(page) {
-        console.log(this.state);
+                });
+        }
         ajax_post(this.props.api, {
             start: (page-1)*this.state.item_per_page + 1,
             end: page*this.state.item_per_page,
@@ -46,7 +55,11 @@ class AdminTable extends Component {
             for(const d of result.data.list) {
                 let new_record = {};
                 for(const c of that.props.columns) {
-                    new_record[c['dataIndex']] = d[c['dataIndex']];
+                    if(d[c['dataIndex']]===undefined) {
+                        new_record[c['dataIndex']] = [];
+                    } else {
+                        new_record[c['dataIndex']] = d[c['dataIndex']];
+                    }
                 }
                 that.data.push(new_record);
             }
@@ -82,63 +95,312 @@ class AdminTable extends Component {
     componentDidMount() {
         this.updateTable(1);
     }
+    componentDidUpdate(prevProps) {
+        if(prevProps === this.props)
+            return;
+        this.setState({
+            loading: true,
+        });
+        this.updateTable(1);
+    }
     render() {
         return (
             <Table columns={this.props.columns}
                    dataSource={this.state.data}
-                   scroll={{ x: 1300 }}
+                   scroll={{ x: this.props.scroll_x }}
                    pagination={this.state.pagination}
                    loading={this.state.loading}
-                   onChange={this.handleTableChange} />
+                   onChange={this.handleTableChange}/>
         );
     }
 }
 
+const scroll_x = {
+    'Users': 1300,
+    'Courses': 1150,
+    'Homeworks': 600,
+    'Problems': 1450,
+    'Records': 1750,
+    'Notices': 600,
+};
+
 const table_api = {
     'Users': api_list['list_user'],
+    'Courses': api_list['list_course'],
+    'Homework': api_list['list_homework'],
+    'Problems': api_list['list_problem'],
+    'Records': api_list['list_record'],
+    'Notices': api_list['list_notice']
 };
 
 const table_delete_api = {
     'Users': api_list['delete_user'],
+    'Courses': api_list['delete_course'],
+    'Homework': api_list['delete_homework'],
+    'Problems': api_list['delete_problem'],
+    'Records': api_list['delete_record'],
+    'Notices': api_list['delete_notice'],
 };
+
+function timeConverter(UNIX_timestamp){
+    let a = new Date(UNIX_timestamp * 1000);
+    let months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    let year = a.getFullYear();
+    let month = months[a.getMonth()];
+    let date = a.getDate();
+    let hour = a.getHours();
+    let min = a.getMinutes();
+    let sec = a.getSeconds();
+    return date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec;
+}
+
+const result_arr = ['Accepted',
+    'Wrong Answer',
+    'Runtime Error',
+    'Time Limit Exceed',
+    'Memory Limit Exceed',
+    'Output Limit Exceed',
+    'Danger System Call',
+    'Judgement Failed',
+    'Compile Error',
+    'unknown',
+];
 
 const table_columns = {
     'Users': [
         {title: 'ID', dataIndex: 'id', key: 'id', fixed: 'left'},
-        {title: 'Username', dataIndex: 'username', key: 'username', fixed: 'left'},
+        {title: '用户名', dataIndex: 'username', key: 'username', fixed: 'left'},
         {title: 'Email', dataIndex: 'email', key: 'email', width: 150},
-        {title: 'Real Name', dataIndex: 'realname', key: 'realname', width: 150},
-        {title: 'Student ID', dataIndex: 'student_id', key: 'student_id', width: 150},
-        {title: 'Gender', dataIndex: 'gender', key: 'gender', width: 150, render: (num) => {
+        {title: '真实姓名', dataIndex: 'realname', key: 'realname', width: 150},
+        {title: '学号', dataIndex: 'student_id', key: 'student_id', width: 150},
+        {title: '性别', dataIndex: 'gender', key: 'gender', width: 150, render: (num) => {
             switch(num) {
-                case 0:
-                    return <span>Male</span>
-                case 1:
-                    return <span>Female</span>
-                case 2:
-                    return <span>Unknown</span>
+                case 0: return <span>男</span>;
+                case 1: return <span>女</span>;
+                case 2: return <span>未知</span>;
+                default: return <span>读取数据错误</span>;
             }
         }},
-        {title: 'Role', dataIndex: 'role', key: 'role', width: 150, render: (num) => {
+        {title: '角色', dataIndex: 'role', key: 'role', width: 150, render: (num) => {
             switch(num) {
-                case 1:
-                    return <span>Student</span>
-                case 2:
-                    return <span>Teaching Assistant</span>
-                case 3:
-                    return <span>Administrator</span>
+                case 1: return <span>学生</span>;
+                case 2: return <span>助教</span>;
+                case 3: return <span>管理员</span>;
+                default: return <span>读取数据错误</span>;
             }
         }},
-        {title: 'Student Courses', dataIndex: 'student_courses', key: 'student_courses', width: 150, render: (courses) =>
-            <span>
-                [{courses.map((c)=>{return c+','})}]
-            </span>
-        },
-        {title: 'TA Courses', dataIndex: 'ta_courses', key: 'ta_courses', width: 150, render: (courses) =>
-                <span>
-                [{courses.map((c)=>{return c+','})}]
-            </span>
-        }
+        {title: '学生课程', dataIndex: 'student_courses', key: 'student_courses', width: 150, render: (courses) => {
+            if(courses === undefined) {
+                courses = [];
+            }
+            return (<span>[{courses.map((c) => {return c + ','})}]</span>);
+        }},
+        {title: '助教课程', dataIndex: 'ta_courses', key: 'ta_courses', width: 150, render: (courses) => {
+            if(courses === undefined) {
+                courses = [];
+            }
+            return (<span>[{courses.map((c) => {return c + ','})}]</span>);
+        }}
+    ],
+    'Courses': [
+        {title: 'ID', dataIndex: 'id', key: 'id', fixed: 'left'},
+        {title: '课堂名称', dataIndex: 'name', key: 'name', fixed: 'left'},
+        {title: '课程描述', dataIndex: 'description', key: 'description', width: 150},
+        {title: '状态', dataIndex: 'status', key: 'status', width: 150, render: (num) => {
+            switch(num) {
+                case 0: return <span>未发布</span>;
+                case 1: return <span>已发布</span>;
+                default: return <span>读取数据错误</span>;
+            }
+        }},
+        {title: '助教', dataIndex: 'tas', key: 'tas', width: 150, render: (tas) => {
+            if(tas===undefined) {
+                tas = [];
+            }
+            return (<span>[{tas.map((c) => {return c + ','})}]</span>);
+        }},
+        {title: '学生', dataIndex: 'students', key: 'students', width: 150, render: (sts) => {
+            if(sts === undefined) {
+                sts = [];
+            }
+            return (<span>[{sts.map((c)=>{return c+','})}]</span>);
+        }},
+        {title: '作业', dataIndex: 'homeworks', key: 'homeworks', width: 150, render: (hws) => {
+            if(hws === undefined) {
+                hws = [];
+            }
+            return (<span>[{hws.map((c)=>{return c+','})}]</span>);
+        }},
+        {title: '通知', dataIndex: 'notices', key: 'notices', width: 150, render: (nts) => {
+            if(nts === undefined) {
+                nts = [];
+            }
+            return (<span>[{nts.map((c)=>{return c+','})}]</span>);
+        }},
+    ],
+    'Homework': [
+        {title: 'ID', dataIndex: 'id', key: 'id', width: 150},
+        {title: '作业名称', dataIndex: 'name', key: 'name', width: 150},
+        {title: '作业描述', dataIndex: 'description', key: 'description', width: 150},
+        {title: '截止日期', dataIndex: 'deadline', key: 'deadline', width: 150, render: (timestamp) => {
+            if(timestamp === undefined) {
+                return <span>读取数据错误</span>;
+            }
+            return <span>{timeConverter(timestamp)}</span>;
+        }},
+        {title: '包含题目', dataIndex: 'problems', key: 'problems', width: 150, render: (pbs) => {
+            if(pbs === undefined) {
+                pbs = [];
+            }
+            return (<span>[{pbs.map((c)=>{return c+','})}]</span>);
+        }},
+        {title: '状态', dataIndex: 'status', key: 'status', width: 150, render: (status) => {
+            switch(status) {
+                case 0: return <span>未开始评测</span>;
+                case 1: return <span>正在评测</span>;
+                case 2: return <span>评测完成</span>;
+                default: return <span>读取数据错误</span>;
+            }
+        }},
+    ],
+    'Problems': [
+        {title: 'ID', dataIndex: 'id', key: 'id', fixed: 'left'},
+        {title: '标题', dataIndex: 'title', key: 'title', fixed: 'left'},
+        {title: '创建者', dataIndex: 'user_id', key: 'user_id', width: 150},
+        {title: '题目描述', dataIndex: 'description', key: 'description', width: 150},
+        {title: '时间限制', dataIndex: 'time_limit', key: 'time_limit', width: 150, render: (time) => {
+            if(time === undefined) {
+                return <span>读取数据错误</span>;
+            }
+            return <span>{time + 'ms'}</span>;
+        }},
+        {title: '内存限制', dataIndex: 'memory_limit', key: 'memory_limit', width: 150, render: (memory) => {
+            if(memory === undefined) {
+                return <span>读取数据错误</span>;
+            }
+            return <span>{memory + 'kb'}</span>;
+        }},
+        {title: '评测方式', dataIndex: 'judge_method', key: 'judge_method', width: 150, render: (method) => {
+            switch(method) {
+                case 0: return <span>传统评测</span>;
+                case 1: return <span>脚本评测</span>;
+                default: return <span>读取数据错误</span>;
+            }
+        }},
+        {title: '允许使用的语言', dataIndex: 'language', key: 'language', width: 150, render: (language) => {
+            if(language === undefined) {
+                language = [];
+            }
+            return <span>[{language.map((lang)=>{
+                switch(lang) {
+                    case 1: return 'C,';
+                    case 2: return 'C++,';
+                    case 3: return 'Javascript';
+                    case 4: return 'Python3';
+                    default: return '未知语言';
+                }
+            })}]</span>;
+        }},
+        {title: '开放性', dataIndex: 'openness', key: 'openness', width: 150, render: (openness) => {
+            switch(openness) {
+                case 0: return <span>私密</span>;
+                case 1: return <span>公开</span>;
+                default: return <span>读取数据错误</span>;
+            }
+        }},
+        {title: '状态', dataIndex: 'status', key: 'status', width: 150, render: (status) => {
+            switch(status) {
+                case 0: return <span>标准程序未通过测试</span>;
+                case 1: return <span>标准程序通过测试</span>;
+                default: return <span>读取数据错误</span>;
+            }
+        }},
+    ],
+    'Records': [
+        {title: 'ID', dataIndex: 'id', key: 'id', fixed: 'left'},
+        {title: '提交用户', dataIndex: 'user_id', key: 'user_id', fixed: 'left'},
+        {title: '提交时间', dataIndex: 'submit_time', key: 'submit_time', width: 150, render: (time) => {
+            if(time === undefined || time === null) {
+                return <span>读取数据错误</span>;
+            }
+            return <span>{timeConverter(time)}</span>;
+        }},
+        {title: '题目', dataIndex: 'problem_id', key: 'problem_id', width: 100},
+        {title: '所属作业', dataIndex: 'homework_id', key: 'homework_id', width: 100},
+        {title: '提交语言', dataIndex: 'src_language', key: 'src_language', width: 100, render: (language) => {
+            switch(language) {
+                case 1: return <span>C</span>;
+                case 2: return <span>C++</span>;
+                case 3: return <span>Javascript</span>;
+                case 4: return <span>Python3</span>;
+                default: return <span>未知语言</span>;
+            }
+        }},
+        {title: '记录类型', dataIndex: 'record_type', key: 'record_type', width: 150, render: (type) => {
+            switch(type) {
+                case 0: return <span>公共图库提交</span>;
+                case 1: return <span>课程作业测试</span>;
+                case 2: return <span>课程作业提交</span>;
+                default: return <span>读取数据错误</span>;
+            }
+        }},
+        {title: '测试比例', dataIndex: 'test_ratio', key: 'test_ratio', width: 100, render: (ratio) => {
+            if(ratio === undefined || ratio === null) {
+                return <span>无数据</span>;
+            }
+            return <span>{ratio + '%'}</span>;
+        }},
+        {title: '记录状态', dataIndex: 'status', key: 'status', width: 150, render: (status) => {
+            switch(status) {
+                case 0: return <span>未完成评测</span>;
+                case 1: return <span>已完成评测</span>;
+                default: return <span>读取数据错误</span>;
+            }
+        }},
+        {title: '传统评测结果', dataIndex: 'result', key: 'result', width: 100, render: (result) => {
+            if(isNaN(result) || result === undefined || result === null) {
+                return <span>无数据</span>;
+            }
+            return <span>{result_arr[result]}</span>;
+        }},
+        {title: '脚本评测结果', dataIndex: 'score', key: 'score', width: 100, render: (score) => {
+            if(isNaN(score) || score === undefined || score === null) {
+                return <span>无数据</span>;
+            }
+            return <span>{score+'分'}</span>;
+        }},
+        {title: '结果类型', dataIndex: 'result_type', key: 'result_type', width: 150, render: (type) => {
+            switch(type) {
+                case 0: return <span>编译型结果</span>;
+                case 1: return <span>脚本测试结果</span>;
+                default: return <span>读取数据错误</span>;
+            }
+        }},
+        {title: '消耗时间', dataIndex: 'consume_time', key: 'consume_time', width: 100, render: (time) => {
+            if(time === undefined || time === null) {
+                return <span>无数据</span>;
+            }
+            return <span>{time + 'ms'}</span>;
+        }},
+        {title: '消耗内存', dataIndex: 'consume_memory', key: 'consume_memory', width: 100, render: (memory) => {
+            if(memory === undefined || memory === null) {
+                return <span>无数据</span>;
+            }
+            return <span>{memory + 'kb'}</span>;
+        }},
+        {title: '源码大小', dataIndex: 'src_size', key: 'src_size', width: 100, render: (size) => {
+            if(size === undefined || size === null) {
+                return <span>无数据</span>;
+            }
+            return <span>{size + 'B'}</span>;
+        }},
+    ],
+    'Notices': [
+        {title: 'ID', dataIndex: 'id', key: 'id', width: 150},
+        {title: '标题', dataIndex: 'title', key: 'title', width: 150},
+        {title: '正文', dataIndex: 'content', key: 'content', width: 250},
+        {title: '创建用户', dataIndex: 'user_id', key: 'user_id', width: 150},
+        {title: '相关课程', dataIndex: 'course_id', key: 'course_id', width: 150},
     ]
 };
 
@@ -148,7 +410,7 @@ class AdminPage extends Component {
         this.state = {
             current: 'Users'
         };
-        this.tables = ['Users', 'Courses', 'Homeworks', 'Problems', 'Records', 'Notices'];
+        this.tables = ['Users', 'Courses', 'Homework', 'Problems', 'Records', 'Notices'];
     }
     handleClick = (e) => {
         this.setState({
@@ -176,7 +438,9 @@ class AdminPage extends Component {
                         <Content style={{ padding: '0 24px', minHeight: 280 }}>
                             <AdminTable columns={table_columns[this.state.current]}
                                         api={table_api[this.state.current]}
-                                        delete_api={table_delete_api[this.state.current]}/>
+                                        delete_api={table_delete_api[this.state.current]}
+                                        scroll_x={scroll_x[this.state.current]}
+                                        current_tab={this.state.current}/>
                         </Content>
                     </Layout>
                 </Content>
--- frontend/src/ajax-utils/api-manager.js
+++ frontend/src/ajax-utils/api-manager.js
@@ -12,6 +12,7 @@ const api_list = {
     'validate_user': '/api/user/validate',
     'update_user': '/api/user/update',
 
+    'list_course': '/api/course/list',
     'create_course': '/api/course/create',
     'delete_course': '/api/course/delete',
     'update_course': '/api/course/update',
@@ -21,27 +22,30 @@ const api_list = {
     'deleteStudent_course': '/api/course/deleteStudent',
     'deleteTA_course': '/api/course/deleteTA',
 
+    'list_homework': '/api/homework/list',
     'create_homework': '/api/homework/create',
     'delete_homework': '/api/homework/delete',
     'update_homework': '/api/homework/update',
     'query_homework': '/api/homework/query',
 
+    'list_notice': '/api/notice/list',
     'create_notice': '/api/notice/create',
     'delete_notice': '/api/notice/delete',
     'update_notice': '/api/notice/update',
     'query_notice': '/api/notice/query',
 
+    'list_record': '/api/record/list',
     'create_record': '/api/record/create',
     'delete_record': '/api/record/delete',
     'query_record': '/api/record/query',
     'srcCode_record': '/api/record/srcCode',
 
+    'list_problem': '/api/problem/list',
     'create_problem': '/api/problem/create',
     'delete_problem': '/api/problem/delete',
     'update_problem': '/api/problem/update',
     'query_problem': '/api/problem/query',
-    'submit_problem': '/api/problem/submit',
-    'list_problem': '/api/problem/list'
+    'submit_problem': '/api/problem/submit'
 };
 
 export {URL, api_list}
