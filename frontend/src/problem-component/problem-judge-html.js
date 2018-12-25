9243545db7d88272a41534caec168100f7200d6c
ycdfwzy <ycdfwzy@outlook.com>
2018-12-25 22:15:48 +0800

    try to solve charser bugs

--- frontend/src/problem-component/problem-judge-html.js
+++ frontend/src/problem-component/problem-judge-html.js
@@ -137,6 +137,9 @@ class mJudgeHTML extends Component {
             <div style={{height: '100%'}}>
                 <div style={{height: '95%'}}>
                     <iframe src={iframe_src} width={'100%'} height={'100%'} sandbox={'allow-scripts'}>
+                        <head>
+                            <meta http-equiv="Content-Type" content="text/html; charset=gbk" />
+                        </head>
                     </iframe>
                 </div>
                 <div style={{height: '4%', marginTop: '1%'}}>
