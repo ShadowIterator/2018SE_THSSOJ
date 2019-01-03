2ee8a24b1c8fd5a49729ac8dea7a623e5de343e1
hongfz <hongfz16@163.com>
2019-01-03 22:53:24 +0800

    fix user input url

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+printf "Please input your domain: "
+read domain
 printf "Please input database name: "
 read db_database
 printf "Please input database user name: "
@@ -7,11 +9,12 @@ read db_user
 printf "Please input password: "
 read -s db_password
 
-secret=$(LC_CTYPE=C tr -dc A-Za-z0-9 < /dev/urandom | fold -w ${1:-32} | head -n 1)
+secret=$(LC_CTYPE=C tr -dc A-Za-z0-9 < /dev/urandom | fold -w ${1:-128} | head -n 1)
 
 printf "POSTGRES_USER=$db_user\nPOSTGRES_PASSWORD=$db_password\nPOSTGRES_DB=$db_database\n" > .env
 printf "db_database = '$db_database'\ndb_user = '$db_user'\ndb_password = '$db_password'\njudgerSecret= '$secret'\n" > backend/settings/env_config.py
 printf "domain = 'http://tornado_web:8000'\nsecret = '$secret'\n" > judger/configs.py
+printf "const URL = '$domain';\nexport {URL};\n" > frontend/src/ajax-utils/url.js
 
 printf "Configuration files successfully generated.\nNow you can run the following command to get the system start up.\n"
 printf "(sudo) docker-compose down\n(sudo) docker-compose build\n(sudo) docker-compose up\n"
